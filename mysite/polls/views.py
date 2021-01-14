57c60ce56768d4dc5cb242eb926728228dfd72d5
KYUNGHEE YANG <alicesister1@gmail.com>
2021-01-15 00:28:24 +0900

    feat: add detail, result templates, views

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,9 @@
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
 
-from .models import Question
+from .models import Question, Choice
 
 
 def index(request):
@@ -19,8 +20,24 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    return HttpResponse("You're looking at results of question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    # return HttpResponse("You're looking at results of question %s. votes count" % question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # details 투표 폼을 다시 표시
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # 뒤로가기 눌렀을 때 같은 폼이 두번 제출되는 경우 방지를 위해
+        # POST가 성공적으로 끝난 후 항상 HttpResponseRedirect를 반환
+        return HttpResponseRedirect(reverse('polls:results', args=(question_id,)))
