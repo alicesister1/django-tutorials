f03b1e759be4a862bac5a73da5ce2a447d651b1a
KYUNGHEE YANG <alicesister1@gmail.com>
2021-01-19 23:53:44 +0900

    test: Add index view test

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -1,6 +1,7 @@
 import datetime
 
 from django.test import TestCase
+from django.urls import reverse
 from django.utils import timezone
 
 from .models import Question
@@ -23,3 +24,68 @@ class QuestionModeTest(TestCase):
         time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
         recent_question = Question(pub_date=time)
         self.assertIs(recent_question.was_published_recently(), True)
+
+
+def create_question(question_text, days):
+    """
+    주어진 일자와 질문 내용으로 Questions 생성한다.
+    일자가 (-)이면 현재보다 과거시점에 게제된 것이고,
+    일자가 (+)이면 미래시점으로 해당 날자에 게제되어야 함을 나타낸다.
+    """
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text, pub_date=time)
+
+
+class QuestionIndexViewTests(TestCase):
+    def test_no_questions(self):
+        """
+        질문이 존재하지 않으면 적절한 메세지가 표시된다.
+        """
+        response = self.client.get(reverse('polls:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
+    def test_past_question(self):
+        """
+        pub_date 가 과거시점인 경우 인덱스에 표시된다.
+        """
+        create_question(question_text="Past question.", days=-30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_question_list'],
+            ['<Question: Past question.>']
+        )
+
+    def test_future_question(self):
+        """
+        pub_date 가 미래시점인 경우 인덱스 페이지에 표시되지 않는다.
+        """
+        create_question(question_text="Future question", days=30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
+    def test_future_question_and_past_question(self):
+        """
+        pub_date 가 과거시점, 미래시점인 서로 다른 질문이 있더라도 과거시점의 질문만 표시된다.
+        """
+        create_question(question_text="Past question.", days=-30)
+        create_question(question_text="Future question.", days=30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_question_list'],
+            ['<Question: Past question.>']
+        )
+
+    def test_two_past_questions(self):
+        """
+        인덱스 페이지는 여러개 질문을 표시할 것이다.
+        """
+        create_question(question_text="Past question 1.", days=-30)
+        create_question(question_text="Past question 2.", days=-5)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_question_list'],
+            ['<Question: Past question 2.>', '<Question: Past question 1.>']
+        )
